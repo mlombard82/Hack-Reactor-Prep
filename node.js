93e3c1e8627d5f3ceca24b55589fdb788a670c72
mlombard82 <mlombard82@gmail.com>
2016-03-01 10:11:44 -0500

    creating  node.js notes

---
+++ node.js
@@ -0,0 +1,13 @@
+
+Node.js is a platform for running JavaScript on the server. Rather than use server-side 
+languages like PHP or Python, you can use JavaScript. After introducing Node.js, Brian 
+walks through a “Hello World” exercise.
+
+node.js takes Google Chrome’s JavaScript engine (V8) and uses it to control libenv, a library
+ that allows you to create and run a server. It’s super rad and pretty fast.
+
+ We’re going to use JS to code the server. This is useful because you only need to learn
+  one language
+You’re using node.js instead of Python, Ruby, Java, PHP, etc.
+node.js is quite different from other server-side languages.
+
